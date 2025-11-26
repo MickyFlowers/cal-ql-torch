9d71bb587b4f209c6ad7098d8b78532edf9dfff3
cyx <cyx010402@gmail.com>
2025-11-26 16:17:44 +0800

    add rollout online .sh

---
+++ scripts/rollout_online.sh
@@ -0,0 +1,9 @@
+python -m rollout.rollout_online \
+    port=603 \
+    host_name="120.48.58.215" \
+    user_name="root" \
+    key_filepath="/home/cyx/.ssh/id_rsa" \
+    remote_data_dir="/mnt/pfs/datasets/online_rl_data_11_26_15/" \
+    remote_online_ckpt_dir="/root/workspace/cal-ql-torch/" \
+    remote_offline_ckpt="/root/workspace/cal-ql-torch/checkpoints/offline_20251125_191534/checkpoint_00300.pt" \
+    
