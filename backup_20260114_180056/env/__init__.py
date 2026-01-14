35ed85a1eb1ab3ce03aef5e899285a40505f8560
cyx <cyx010402@gmail.com>
2026-01-14 19:00:16 +0800

    remove backup directory from 20260114_180056

--- backup_20260114_180056/env/__init__.py
+++
@@ -1,3 +0,0 @@
-from gym.envs.registration import register
-
-register(id="ur_env_v0", entry_point="env.env:UrEnv")
--- backup_20260114_180056/tests/teach_world_pose.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from xlib.algo.utils.transforms import applyDeltaPose6d, matrixToPose6d
-from xlib.device.manipulator import UR
-
-ip = "172.16.11.233"
-base_to_world = np.load("assets/left_base_to_world.npy")
-ur_robot = UR(ip, matrixToPose6d(base_to_world))
-print(ur_robot.world_pose)
